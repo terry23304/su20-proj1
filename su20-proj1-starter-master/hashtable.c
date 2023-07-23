a00e37de9bf48806ef3397b236fa3657cf10bb46
terry23304 <p76111068@gmail.com>
2023-07-23 19:44:37 +0800

    Finished

---
+++ su20-proj1-starter-master/hashtable.c
@@ -0,0 +1,75 @@
+#include "hashtable.h"
+#include <stdlib.h>
+
+/*
+ * This creates a new hash table of the specified size and with
+ * the given hash function and comparison function.
+ */
+HashTable *createHashTable(int size, unsigned int (*hashFunction)(void *),
+                           int (*equalFunction)(void *, void *)) {
+  int i = 0;
+  HashTable *newTable = malloc(sizeof(HashTable));
+  newTable->size = size;
+  newTable->data = malloc(sizeof(struct HashBucket *) * size);
+  for (i = 0; i < size; ++i) {
+    newTable->data[i] = NULL;
+  }
+  newTable->hashFunction = hashFunction;
+  newTable->equalFunction = equalFunction;
+  return newTable;
+}
+
+/*
+ * This inserts a key/data pair into a hash table.  To use this
+ * to store strings, simply cast the char * to a void * (e.g., to store
+ * the string referred to by the declaration char *string, you would
+ * call insertData(someHashTable, (void *) string, (void *) string).
+ * Because we only need a set data structure for this spell checker,
+ * we can use the string as both the key and data.
+ */
+void insertData(HashTable *table, void *key, void *data) {
+  // -- TODO --
+  // HINT:
+  // 1. Find the right hash bucket location with table->hashFunction.
+  // 2. Allocate a new hash bucket struct.
+  // 3. Append to the linked list or create it if it does not yet exist. 
+  unsigned int hash = table->hashFunction(key);
+  struct HashBucket *newBucket = malloc(sizeof(struct HashBucket));
+  newBucket->key = key;
+  newBucket->data = data;
+  newBucket->next = NULL;
+
+  if (!table->data[hash]) {  
+    table->data[hash] = newBucket;
+    return;
+  }
+
+  while (table->data[hash]->next)
+    table->data[hash] = table->data[hash]->next;
+  
+  table->data[hash]->next = newBucket;
+  
+  return;
+}
+
+/*
+ * This returns the corresponding data for a given key.
+ * It returns NULL if the key is not found. 
+ */
+void *findData(HashTable *table, void *key) {
+  // -- TODO --
+  // HINT:
+  // 1. Find the right hash bucket with table->hashFunction.
+  // 2. Walk the linked list and check for equality with table->equalFunction.
+  unsigned int hash = table->hashFunction(key);
+  struct HashBucket *bucket = table->data[hash];
+
+  while (bucket) {
+    if (table->equalFunction(bucket->key, key))
+      return bucket->data;
+    
+    bucket = bucket->next;
+  }
+
+  return NULL;
+}
---
+++ su20-proj1-starter-master/philspel.c
@@ -0,0 +1,212 @@
+/*
+ * Include the provided hash table library.
+ */
+#include "hashtable.h"
+
+/*
+ * Include the header file.
+ */
+#include "philspel.h"
+
+/*
+ * Standard IO and file routines.
+ */
+#define _GNU_SOURCE
+#include <stdio.h>
+
+/*
+ * General utility routines (including malloc()).
+ */
+#include <stdlib.h>
+
+/*
+ * Character utility routines.
+ */
+#include <ctype.h>
+
+/*
+ * String utility routines.
+ */
+#include <string.h>
+
+/*
+ * This hash table stores the dictionary.
+ */
+HashTable *dictionary;
+
+/*
+ * The MAIN routine.  You can safely print debugging information
+ * to standard error (stderr) as shown and it will be ignored in 
+ * the grading process.
+ */
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Specify a dictionary\n");
+    return 0;
+  }
+  /*
+   * Allocate a hash table to store the dictionary.
+   */
+  fprintf(stderr, "Creating hashtable\n");
+  dictionary = createHashTable(2255, &stringHash, &stringEquals);
+
+  fprintf(stderr, "Loading dictionary %s\n", argv[1]);
+  readDictionary(argv[1]);
+  fprintf(stderr, "Dictionary loaded\n");
+
+  fprintf(stderr, "Processing stdin\n");
+  processInput();
+
+  /*
+   * The MAIN function in C should always return 0 as a way of telling
+   * whatever program invoked this that everything went OK.
+   */
+  return 0;
+}
+
+/*
+ * This should hash a string to a bucket index.  Void *s can be safely cast
+ * to a char * (null terminated string) and is already done for you here 
+ * for convenience.
+ */
+unsigned int stringHash(void *s) {
+  char *string = (char *)s;
+  // -- TODO --
+  unsigned int hash = 0;
+  int i = 0;
+  while (string[i] != '\0') {
+    hash += string[i];
+    i++;
+  }
+
+  return hash;
+}
+
+/*
+ * This should return a nonzero value if the two strings are identical 
+ * (case sensitive comparison) and 0 otherwise.
+ */
+int stringEquals(void *s1, void *s2) {
+  char *string1 = (char *)s1;
+  char *string2 = (char *)s2;
+  // -- TODO --
+  int i = 0;
+  int length1 = strlen(string1), length2 = strlen(string2);
+
+  if (length1 != length2)
+    return 0;
+  
+  while (string1[i] != '\0') {
+    if (string1[i] != string2[i])
+      return 0;
+    i++;
+  }
+
+  return i;
+}
+
+/*
+ * This function should read in every word from the dictionary and
+ * store it in the hash table.  You should first open the file specified,
+ * then read the words one at a time and insert them into the dictionary.
+ * Once the file is read in completely, return.  You will need to allocate
+ * (using malloc()) space for each word.  As described in the spec, you
+ * can initially assume that no word is longer than 60 characters.  However,
+ * for the final 20% of your grade, you cannot assumed that words have a bounded
+ * length.  You CANNOT assume that the specified file exists.  If the file does
+ * NOT exist, you should print some message to standard error and call exit(1)
+ * to cleanly exit the program.
+ *
+ * Since the format is one word at a time, with new lines in between,
+ * you can safely use fscanf() to read in the strings until you want to handle
+ * arbitrarily long dictionary chacaters.
+ */
+void readDictionary(char *dictName) {
+  // -- TODO --
+  FILE *fp = fopen(dictName, "r");
+  if (!fp) {
+    fprintf(stderr, "File does not exist\n");
+    exit(1);
+  }
+
+  char *line = NULL;
+  size_t len = 0, read;
+  
+  while ((read = getline(&line, &len, fp)) != -1) {
+    char *word = malloc(sizeof(char) * read);
+    strncpy(word, line, (read - 1));
+
+    insertData(dictionary, word, word);
+  }
+}
+
+/*
+ * This should process standard input (stdin) and copy it to standard
+ * output (stdout) as specified in the spec (e.g., if a standard 
+ * dictionary was used and the string "this is a taest of  this-proGram" 
+ * was given to stdin, the output to stdout should be 
+ * "this is a teast [sic] of  this-proGram").  All words should be checked
+ * against the dictionary as they are input, then with all but the first
+ * letter converted to lowercase, and finally with all letters converted
+ * to lowercase.  Only if all 3 cases are not in the dictionary should it
+ * be reported as not found by appending " [sic]" after the error.
+ *
+ * Since we care about preserving whitespace and pass through all non alphabet
+ * characters untouched, scanf() is probably insufficent (since it only considers
+ * whitespace as breaking strings), meaning you will probably have
+ * to get characters from stdin one at a time.
+ *
+ * Do note that even under the initial assumption that no word is longer than 60
+ * characters, you may still encounter strings of non-alphabetic characters (e.g.,
+ * numbers and punctuation) which are longer than 60 characters. Again, for the 
+ * final 20% of your grade, you cannot assume words have a bounded length.
+ */
+void processInput() {
+  // -- TODO --
+
+  char *word = malloc(sizeof(char) * 60);
+  int i = 0;
+  char c = getchar();
+
+  while (c != EOF) {
+    if (isalpha(c))
+      word[i++] = c;
+    else {
+      word[i] = '\0';
+      
+      if (i == 0)
+        goto skip;
+
+      // 原本的詞找一次，除了第一個轉小寫再找一次，全部轉小寫再找一次
+      if (!findData(dictionary, word)) {
+        char *lowercase = malloc(sizeof(char) * i);
+        strncpy(lowercase, word, i);
+        lowercase[i] = '\0';
+
+        int j = 1;
+        while (lowercase[j] != '\0') {
+          lowercase[j] = tolower(lowercase[j]);
+          j++;
+        }
+
+        if (!findData(dictionary, lowercase)) {
+          lowercase[0] = tolower(lowercase[0]);
+
+          if (!findData(dictionary, lowercase))
+            printf("%s [sic]", word);
+          else
+            printf("%s", word);
+        }
+        else
+          printf("%s", word);
+      }
+      else
+        printf("%s", word);
+
+      skip:
+      printf("%c", c);
+      i = 0;
+    }
+    c = getchar();
+  }
+}
